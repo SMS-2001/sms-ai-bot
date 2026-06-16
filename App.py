ea8a89a0e3e6b7f4be63ccbf1c231374c3ed7abd
Sunil Maurya <sushilmaurya184@gmail.com>
2026-06-16 12:59:48 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+API_KEY = os.getenv("API_KEY")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_msg = request.json["message"]
+
+    if not API_KEY:
+        return jsonify({"reply": "API Key missing!"})
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
+
+    payload = {
+        "contents": [{
+            "parts": [{"text": user_msg}]
+        }]
+    }
+
+    try:
+        response = requests.post(url, json=payload)
+        data = response.json()
+
+        reply = data["candidates"][0]["content"]["parts"][0]["text"]
+    except:
+        reply = "Sorry 😢 AI not responding right now."
+
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
